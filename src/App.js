cad08fb4c2531d3b5179a57b39fdae95d33bc016
Jay Nguyen <jay@Jays-iMac.local>
2022-03-14 14:28:33 -0700

    Added pseudocode to explain useEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// useEffect (READ: SIDE EFFECT); WHEN AN ELEMENT IN ITS SECOND ARGUMENT (THE ARRAY) IS MODIFIED, useEffect WILL RE-RENDER THE FIRST ARGUMENT; I.E. A SIDE EFFECT OF THE SECOND ARGUMENT UPDATING IS THE FIRST ARGUMENT UPDATING AS WELL.
 import React, { useState, useRef, useEffect } from 'react';
 import ToDoList from './Components/ToDoList';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,21 +11,30 @@ function App() {
 
 	// IF THERE ARE TODOS, THIS WILL GET THEM
 	useEffect(() => {
+		// LOCAL_STORAGE_KEY IS A STRING; toDos IS AN ARRAY, ERGO, MUST PARSE
 		const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 		if (storedToDos) setToDos(storedToDos);
+		// BECAUSE THE ARRAY BELOW IS EMPTY, USE EFFECT WILL ONLY RUN ONCE: ON PAGE LOAD.
 	}, []);
 
-	// THIS WILL STORE TODOS
+	// THIS WILL STORE TODOS EVERY TIME A TODO ITEM IS ADDED
 	useEffect(() => {
+		// MUST PASS INTO LOCAL STORAGE AS A STRING
 		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDos));
+		// EVERY TIME toDos CHANGES, USE EFFECT WILL RUN.
 	}, [toDos]);
 
+	// THIS WILL ADD A TODO ITEM
 	function handleAddToDo(e) {
+		// THIS REFERENCES THE INPUT FIELD VALUE
 		const name = toDoNameRef.current.value;
+		// IF THE INPUT FIELD IS BLANK UPON ENTERING, NOTHING IS RETURNED
 		if (name === '') return;
+		// THIS SETS THE TODO LIST WITH FORMER ENTRIES AND THE NEW ENTRY
 		setToDos((prevToDos) => {
 			return [...prevToDos, { id: uuidv4(), name: name, complete: false }];
 		});
+		// THIS WILL CLEAR THE INPUT FIELD AFTER TODO ITEM IS ADDED TO LIST
 		toDoNameRef.current.value = null;
 	}
 
